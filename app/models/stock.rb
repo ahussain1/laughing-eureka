f53dbab15b38262866e0cad18efeb604fb837986
Arif Hussain <ahussain@ezcater.com>
2016-01-31 09:50:51 -0500

    stock not deleting

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ActiveRecord::Base
 
+	has_many :user_stocks
+	has_many :users, through: :user_stocks
+
 	def self.find_by_ticker(ticker_symbol)
 		where(ticker: ticker_symbol).first
 	end
@@ -18,7 +21,7 @@ class Stock < ActiveRecord::Base
 		return "#{closing_price} (Closing)" if closing_price
 
 		opening_price = StockQuote::Stock.quote(ticker).open
-		return "#{opening_price} (opening_price)" if opening_price 
+		return "#{opening_price} (opening_price)" if opening_price
 		'Unavailable'
 	end
 
